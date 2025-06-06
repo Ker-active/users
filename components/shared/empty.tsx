330cea1ac57b76041352d98676cf6156b0b0e721
nikhea <imonikheaugbodaga@gmail.com>
2025-06-06 10:22:13 +0100

    fix:copy link

--- components/shared/empty.tsx
+++ components/shared/empty.tsx
@@ -12,18 +12,18 @@ interface IProps extends ImageProps {
 
 export const Empty = ({ href, onClick, desc, linkText, ...rest }: IProps) => {
   return (
-    <div className='flex flex-grow items-center gap-4 py-4 justify-center flex-col'>
-      <Image width={100} height={100} {...rest} />
-      <p className='text-[13px] max-w-[415px] mx-auto text-center leading-[20px] text-[#5B5971]'>
+    <div className="flex flex-grow items-center gap-4 py-4 justify-center flex-col">
+      <Image width={100} height={100} {...rest} alt="ker active" />
+      <p className="text-[13px] max-w-[415px] mx-auto text-center leading-[20px] text-[#5B5971]">
         {desc}
         {onClick ? (
-          <button onClick={onClick} className='text-primary underline'>
+          <button onClick={onClick} className="text-primary underline">
             {linkText}
           </button>
         ) : (
           <>
             {linkText && (
-              <Link className='text-primary underline' href={href!}>
+              <Link className="text-primary underline" href={href!}>
                 {linkText}
               </Link>
             )}
